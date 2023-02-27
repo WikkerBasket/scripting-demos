eef45c248f677882e99dd19d0f3fdadf9492471e
Wizard <wizard@magicbook.localdomain>
2023-02-27 13:57:35 -0800

    Added some special changes to how jumping works, now there is downforce to accelerate as the player reaches the peak of their jump; and also shorthopping.

--- Player.gd
+++ Player.gd
@@ -1,9 +1,15 @@
 extends KinematicBody2D
 
-var onTile = Vector2()
-var speed = 250
 
-var gravity = 200
+
+var onTile = Vector2()
+var speed = 350
+var friction = 0.3
+
+var gravity = 1000
+var jumpWeight = 5
+var jumpHeight = 650
+var shortHop = 250
 
 var isJump = true
 onready var onGround = $onGround
@@ -16,17 +22,23 @@ func walk():
 	elif Input.is_action_pressed("right"):
 		onTile.x = speed
 	else:
-		onTile.x = 0
+		onTile.x = lerp(onTile.x, 0, friction)
+	onTile.x = clamp(onTile.x, -speed, speed)
 	move_and_slide(onTile, Vector2(0, -1))
 		
 
 func jump():
-	if Input.is_action_pressed("up") && isJump == false: 
+	if Input.is_action_pressed("jump") && isJump == false: 
 		isJump = true
-		onTile.y = -speed
-	
+		onTile.y = -jumpHeight
+	if Input.is_action_just_released("jump") && onTile.y < -shortHop:
+		if onTile.y < 0:
+			onTile.y += 350
 	if onGround.is_colliding() == true && isJump == true:
 		isJump = false
+		
+	if onGround.is_colliding() == false && isJump == true:
+		onTile.y += jumpWeight
 
 func _process(delta):
 	onTile.y += delta * gravity
